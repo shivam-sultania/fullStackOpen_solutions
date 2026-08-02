e3c5b67cf00dfcf8ba4766596499e936cde23903
shivam sultania <shivamsultania7@gmail.com>
2026-08-02 18:18:32 +0530

    ex. 2.9

--- part_2/phonebook_app/src/App.tsx
+++ part_2/phonebook_app/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',id:0, number:'0000000000'}
-  ]) 
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const[nameFilter, setNameFilter] = useState('')
 
   const handleSubmit = (event:React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -22,8 +26,9 @@ const App = () => {
       alert("You missed a field or two")
       return
     }
-    setPersons(persons.concat({ name: name,id:persons.length, number:newNumber}))
+    setPersons(persons.concat({ name: name, id:persons.length+1, number:newNumber}))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event:React.ChangeEvent<HTMLInputElement>) => {
@@ -34,9 +39,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setNameFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={nameFilter} onChange={handleFilterChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={(handleSubmit)}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -50,7 +63,9 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         <ul>
-          {persons.map((person) => <li key={person.id}>{person.name} {person.number}</li>)}
+          {persons
+          .filter(person => person.name.toLowerCase().includes(nameFilter.trim().toLowerCase()))
+          .map((person) => <li key={person.id}>{person.name} {person.number}</li>)}
         </ul>
       </div>
     </div>
